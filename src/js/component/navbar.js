fcb7fe4b18afb2d2cdfdc674276514d1297f6106
Jonathan Beltran <beltranjhonnathan@gmail.com>
2024-01-08 21:51:09 +0000

    fetch done

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-light bg-light mb-3 p-3">
+            <Link to="/">
+                <span className="navbar-brand mb-0 h1">Starwars</span>
+            </Link>
+            <div className="ml-auto">
+                <Link to="/demo">
+                <div class="nav-item dropdown">
+                    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false"><i class="fa fa-heart"></i></button>
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+                </Link>
+            </div>
+        </nav>
+    );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,56 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from 'react';
+// import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+export const Home = () => {
+	
+    function obtenerPersonajes() {
+        fetch("https://www.swapi.tech/api/planets/1/")
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err))
+    }
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+    useEffect(() => { // // cada vez que quiera ejecutar una funcion ni bien se cargue el componente debo hacer un useEffect, React dice esto va asi! siempre antes del return
+        obtenerPersonajes()
+    }, [])
+    return (
+        <div className="row row-cols-sm-12 row-cols-md-6 p-5">
+            <div className="col">
+                <div className="card">
+                    <img src="https://starwars-visualguide.com/assets/img/characters/1.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">hola.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src="https://starwars-visualguide.com/assets/img/characters/1.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">hola</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src="https://starwars-visualguide.com/assets/img/characters/1.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">hola</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card">
+                    <img src="https://starwars-visualguide.com/assets/img/characters/1.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">hola.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
